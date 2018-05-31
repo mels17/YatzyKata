09dea9336725ff7cc5d3ed7f31b5f4e95d4bd6e0
Mel Vasu <malavika.vasudevan@myob.com>
2018-05-31 14:27:51 +1000

    Corrected pair logic

--- src/test/java/yatzyTest.java
+++ src/test/java/yatzyTest.java
@@ -1,9 +1,7 @@
+import com.sun.java.swing.plaf.windows.WindowsTreeUI;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -52,6 +50,16 @@ public class yatzyTest {
     @Test
     public void scoreIsSumOf2HighestMatchingDice() {
         assertComputeScore("pair", arrayOf(3, 3, 3, 4, 4), 8);
+        assertComputeScore("pair", arrayOf(1, 1, 6, 2, 6), 12);
+        assertComputeScore("pair", arrayOf(3, 3, 3, 4, 1), 6);
+        assertComputeScore("pair", arrayOf(3, 3, 3, 3, 1), 6);
+    }
+
+    @Test
+    public void ifTwoPairsOfDiceWithSameNumberThePlayerScoresTheSumOfTheseDice() {
+        assertComputeScore("two pairs", arrayOf(1, 1, 2, 3, 3), 0);
+        assertComputeScore("two pairs", arrayOf(1, 1, 2, 3, 4), 0);
+        assertComputeScore("two pairs", arrayOf(1, 1, 2, 2, 2), 0);
     }
 
     private int computeScore(String category, List<Integer> rolls) {
@@ -65,20 +73,52 @@ public class yatzyTest {
         commands.put("fives", () -> getSumBasedOnDiceNumber(rolls, 5));
         commands.put("sixes", () -> getSumBasedOnDiceNumber(rolls, 6));
         commands.put("pair", () -> getSumOfHighest(rolls));
+        commands.put("two pairs", () -> sumTwoPairsOfDiceWithSameNumber(rolls));
 
         return commands.getOrDefault(category, () -> 0).getAsInt();
     }
 
+    private int sumTwoPairsOfDiceWithSameNumber(List<Integer> rolls) {
+        List<Integer> distinctNumbers = rolls.stream().distinct().collect(Collectors.toList());
+        if (distinctNumbers.size() < 2) return 0;
+        List<Integer> frequency = distinctNumbers.stream()
+                .map(i -> Collections.frequency(rolls, i)).collect(Collectors.toList());
+        return 0;
+//        if(Collections.frequency(frequency, 2) > 2) return 0;
+
+
+//        return 2 * (distinctNumbers.get(frequency.lastIndexOf(2)) + distinctNumbers.get(frequency.indexOf(2)));
+    }
+
     private int getSumOfHighest(List<Integer> rolls) {
-        int maxOne = rolls.stream()
+        List<Integer> listOfNumbersFoundInPairs = new ArrayList<>();
+        for(int i = 0; i < rolls.size(); i++) {
+            for(int j = i+1; j < rolls.size(); j++) {
+                if(rolls.get(i).equals(rolls.get(j))) {
+                    listOfNumbersFoundInPairs.add(rolls.get(i));
+//                    rolls.remove(rolls.get(j));
+                    rolls.set(j, 0);
+                    break;
+                }
+            }
+            rolls.set(i, 0);
+//            rolls.remove(rolls.get(i));
+        }
+
+
+        return Collections.max(listOfNumbersFoundInPairs) * 2;
+
+
+//        int maxOne = getMaxOfArrayList(rolls);
+//        rolls.set(rolls.indexOf(maxOne), 0);
+//        return getMaxOfArrayList(rolls) + maxOne;
+    }
+
+    private int getMaxOfArrayList(List<Integer> rolls) {
+        return rolls.stream()
                 .mapToInt(i -> i.intValue())
                 .max()
                 .getAsInt();
-        int index = rolls.indexOf(maxOne);
-        rolls.set(index, 0);
-        System.out.println(index);
-        return rolls.stream().mapToInt(i -> i.intValue()).max().getAsInt() + maxOne;
-//        return 0;
     }
 
     private int getSumWhenAllNumberAreTheSameOtherwise0(List<Integer> rolls) {
